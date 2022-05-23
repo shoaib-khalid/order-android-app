bf342528dc5c690250de8ea9024b5d3d01e369d4
Umar Farooq <uf.umar808@gmail.com>
2022-05-23 12:45:27 +0500

    minor fixes

--- app/src/main/java/com/symplified/order/apis/ProductApi.java
+++ app/src/main/java/com/symplified/order/apis/ProductApi.java
@@ -6,17 +6,23 @@ import com.symplified.order.models.product.ProductResponse;
 
 import java.util.Map;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.HeaderMap;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ProductApi {
 
-    @GET("store-categories?page=0&pageSize=20&sortByCol=name&sortingOrder=ASC&name=")
-    Call<CategoryResponse> getCategories(@HeaderMap Map<String, String> headers, @Query("storeId") String storeId);
-
     @GET("stores/{storeId}/products?page=0&pageSize=10&sortByCol=name&sortingOrder=ASC&name=&status=ACTIVE,INACTIVE,OUTOFSTOCK")
     Call<ProductResponse> getProducts(@HeaderMap Map<String, String> headers, @Path("storeId") String storeId);
+
+    @DELETE("stores/{storeId}/products/{productId}")
+    Call<ResponseBody> deleteProduct(@HeaderMap Map<String, String> headers, @Path("storeId") String storeId, @Path("productId") String productId);
+
+    @PUT("stores/{storeId}/products/{productId}")
+    Call<ResponseBody> updateProduct(@HeaderMap Map<String, String> headers, @Path("storeId") String storeId, @Path("productId") String productId);
 }
