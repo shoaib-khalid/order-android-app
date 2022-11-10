d1fc1e2b5dc176275ac83c8d4c68341ba3cddfed
Imran <imran.tariq@kalsym.com>
2022-11-10 15:47:06 +0500

    Remove debugging code from FirebaseHelper

--- app/src/main/java/com/symplified/order/firebase/FirebaseHelper.java
+++ app/src/main/java/com/symplified/order/firebase/FirebaseHelper.java
@@ -1,21 +1,6 @@
 package com.symplified.order.firebase;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.graphics.Color;
-
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.TaskStackBuilder;
-
 import com.google.firebase.messaging.FirebaseMessaging;
-import com.symplified.order.App;
-import com.symplified.order.LoginActivity;
-import com.symplified.order.R;
-
-import java.util.Random;
 
 public class FirebaseHelper {
     /**
