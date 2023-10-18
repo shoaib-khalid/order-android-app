07bd9cc222945cef263f2c2e6d053fd343e9e3c3
imran-ks <imran.tariq@kalsym.com>
2023-10-18 12:04:32 +0500

    Check for permission before starting QR Code scanner

--- app/src/main/java/com/ekedai/merchant/ui/voucher/tabs/VoucherScanFragment.java
+++ app/src/main/java/com/ekedai/merchant/ui/voucher/tabs/VoucherScanFragment.java
@@ -1,6 +1,8 @@
 package com.ekedai.merchant.ui.voucher.tabs;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,8 +10,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.ekedai.merchant.R;
@@ -28,6 +33,13 @@ public class VoucherScanFragment extends Fragment {
 
     private FragmentVoucherScanBinding binding;
     final String TAG = "voucher-fragment";
+    private final ActivityResultLauncher<String> cameraPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    startScan();
+                }
+            });
+
 
     @Override
     public View onCreateView(
@@ -43,30 +55,37 @@ public class VoucherScanFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
         binding.scanVoucherCodeButton.setOnClickListener(v -> {
+            if (ContextCompat.checkSelfPermission(view.getContext(), Manifest.permission.CAMERA) ==
+                    PackageManager.PERMISSION_DENIED) {
+                cameraPermissionLauncher.launch(Manifest.permission.CAMERA);
+                return;
+            }
 
-            GmsBarcodeScanning.getClient(
-                    binding.getRoot().getContext(),
-                    new GmsBarcodeScannerOptions.Builder()
-                            .setBarcodeFormats(Barcode.FORMAT_QR_CODE, Barcode.FORMAT_AZTEC)
-                            .enableAutoZoom()
-                            .build()
-            ).startScan().addOnSuccessListener(barcode -> {
-                try {
-                    Log.d("voucher", "Scanned QR code: " + barcode.getRawValue());
-                    VoucherQrCodeDetails voucherDetails = new Gson().fromJson(barcode.getRawValue(), VoucherQrCodeDetails.class);
-                    Intent intent = new Intent(requireActivity(), VoucherDetailsActivity.class);
-                    intent.putExtra(VoucherDetailsActivity.VOUCHER_DETAILS_KEY, voucherDetails);
-                    startActivity(intent);
-                } catch (JsonSyntaxException ex) {
-                    Toast.makeText(requireActivity(), "Invalid QR Code", Toast.LENGTH_SHORT).show();
-                }
-            }).addOnFailureListener(e -> {
-                String errorMessage = "Failed to scan QR Code: " + e.getLocalizedMessage();
-                Log.e(TAG, errorMessage);
-                Toast.makeText(getActivity(), errorMessage, Toast.LENGTH_SHORT).show();
-            });
+            startScan();
         });
     }
 
-//    public void
+    public void startScan() {
+        GmsBarcodeScanning.getClient(
+                binding.getRoot().getContext(),
+                new GmsBarcodeScannerOptions.Builder()
+                        .setBarcodeFormats(Barcode.FORMAT_QR_CODE, Barcode.FORMAT_AZTEC)
+                        .enableAutoZoom()
+                        .build()
+        ).startScan().addOnSuccessListener(barcode -> {
+            try {
+                Log.d("voucher", "Scanned QR code: " + barcode.getRawValue());
+                VoucherQrCodeDetails voucherDetails = new Gson().fromJson(barcode.getRawValue(), VoucherQrCodeDetails.class);
+                Intent intent = new Intent(requireActivity(), VoucherDetailsActivity.class);
+                intent.putExtra(VoucherDetailsActivity.VOUCHER_DETAILS_KEY, voucherDetails);
+                startActivity(intent);
+            } catch (JsonSyntaxException ex) {
+                Toast.makeText(requireActivity(), "Invalid QR Code", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(e -> {
+            String errorMessage = "Failed to scan QR Code: " + e.getLocalizedMessage();
+            Log.e(TAG, errorMessage);
+            Toast.makeText(getActivity(), errorMessage, Toast.LENGTH_SHORT).show();
+        });
+    }
 }
